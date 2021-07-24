7ee2d745aede360ec805ecfd5b590345d354c24a
Kiran Manicka <kmanicka013@gmail.com>
2021-07-24 01:40:27 -0400

    added battle to smart contract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,4 +2,5 @@ const Card = artifacts.require("Card");
 
 module.exports = function(deployer) {
   deployer.deploy(Card);
+  
 };
--- src/contracts/Card.sol
+++ src/contracts/Card.sol
@@ -4,19 +4,24 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Card is ERC721{
 
-     struct Creature{
+    uint public _id=0;
+    Creature[] public creatures;
+    Battle[] public battles;
+    
+    struct Creature {
         string name;
         string bio;
         string imageURI;
         uint attackPoints;
         uint defensePoints;
         string specialMove;
+        uint value;
     }
-
-    uint public _id=0;
-    Creature[] public creatures;
-    mapping (uint => address) owners;
-
+    
+    struct Battle{
+        address host;
+        uint hostCreature;
+    }
    
 
     constructor() ERC721('Card',"CARD") {
@@ -31,10 +36,10 @@ contract Card is ERC721{
     uint defensePoints,
     string memory specialMove) 
     public {
-        Creature memory what = Creature(name,bio,imageURI,attackPoints,defensePoints,specialMove);
+        Creature memory what = Creature({name:name,bio:bio,imageURI:imageURI
+        ,attackPoints:attackPoints,defensePoints:defensePoints,specialMove:specialMove,value:2000000000000000});
         creatures.push(what);
         _mint(msg.sender, _id);
-        owners[_id]=msg.sender;
         _id=_id+1;
     }
     
@@ -49,6 +54,24 @@ contract Card is ERC721{
             }
         }
         return tokensFromAddress;
+    }
+
+    function createBattle(address host, uint tokenID )public{
+        Battle memory battle= Battle({host:host, hostCreature:tokenID});
+        battles.push(battle);
+    }
+    
+    function joinBattle(uint battleID,uint tokenID) public returns(uint){
+        Battle memory battle= battles[battleID];
+        uint specialNumber= uint(creatures[tokenID].value/(creatures[battle.hostCreature].value+creatures[tokenID].value));
+        uint random = uint(keccak256(abi.encodePacked(battleID,tokenID,block.timestamp,block.difficulty)))%100;
+        if(random<=specialNumber){
+            _transfer(ownerOf(battle.hostCreature),ownerOf(tokenID),battle.hostCreature);
+            return battle.hostCreature;
+        }else{
+            _transfer(ownerOf(tokenID),ownerOf(battle.hostCreature),tokenID);
+            return tokenID;
+        }
     }
     
 
